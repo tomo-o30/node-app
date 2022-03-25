8c3e3091b33ca0db9af18c959eb555bf3920c116
tomo-o30 <tomo.ogawa18@gmail.com>
2022-03-25 15:00:02 +0900

    静的コンテンツ配信　実装

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+const path = require("path");
 const express = require("express");
+const favicon = require("serve-favicon");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.end("Hello world");
-});
+// Express settings
+app.set("view engine", "ejs");
 
+// Static resource rooting.
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+// Dynamic resource rooting.
+app.use("/", require("./routes/index.js"));
+
+// Execute web application.
 app.listen(PORT, () => {
-  console.log(`Application listening at ${PORT}`);
+  console.log(`Application listening at :${PORT}`);
 });
